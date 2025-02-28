dc76782431d7226896fb651d7c0664d75540ae39
dennis-mmachoene <dennismmachoene@gmail.com>
2025-02-28 18:39:01 +0200

    Added NavBar Components

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,19 +5,23 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <header className='py-8 xl:py-12 text-white'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto flex justify-between items-center'>
          <Link href="/">
          <h1 className="text-4xl font-semibold"> 
             Dennis <span className='text-accent'>.</span>
          </h1>
          </Link>
-         <div className="hidden xl:flex">
+         <div className="hidden xl:flex items-center gap-8">
              <Navbar />
              <Link href='/contact'>
              <Button>Hire Me</Button>
              </Link>
          </div>
-        
+
+
+      <div className="xl:hidden">
+        mobile nav
+      </div>
       </div>
     </header>
   )
